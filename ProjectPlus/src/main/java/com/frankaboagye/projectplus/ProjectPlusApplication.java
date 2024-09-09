7950681ff331f131da97f17c6f262280d835e1a7
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-09-09 14:31:02 +0000

    project starter

---
+++ ProjectPlus/src/main/java/com/frankaboagye/projectplus/ProjectPlusApplication.java
@@ -0,0 +1,13 @@
+package com.frankaboagye.projectplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPlusApplication.class, args);
+    }
+
+}
---
+++ ProjectPlus/src/test/java/com/frankaboagye/projectplus/ProjectPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankaboagye.projectplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
