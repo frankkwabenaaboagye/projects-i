298da881a9a49cd396a4ac27f629c60f1ccd9713
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-10-04 12:05:20 +0000

    base files setup for the projects page

--- ConnectHub/src/main/java/com/frankaboagye/connecthub/interfaces/ProjectServiceInterface.java
+++ ConnectHub/src/main/java/com/frankaboagye/connecthub/interfaces/ProjectServiceInterface.java
@@ -5,8 +5,12 @@ import com.frankaboagye.connecthub.entities.Project;
 import jakarta.annotation.Nullable;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface ProjectServiceInterface {
     Project getProjectById(Long id);
     Project updateProject(ProjectDAO projectDAO, Long projectId, Long companyId, MultipartFile documentFile);
-    Project updateProjectWithoutFile(ProjectDAO projectDAO, Long projectId, Long companyId);  // chnage the wording
+    Project updateProjectWithoutFile(ProjectDAO projectDAO, Long projectId, Long companyId);
+    List<Project> getAllProjects();// change the wording
+
 }
