1f2188778a938401eb7b6bd4309c6e7f5624979c
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-10-11 16:27:35 +0000

    add enums - job types - will modify some of them

---
+++ ConnectHub/src/main/java/com/frankaboagye/connecthub/enums/JobType.java
@@ -0,0 +1,12 @@
+package com.frankaboagye.connecthub.enums;
+
+public enum JobType {
+    FULL_TIME,
+    PART_TIME,
+    INTERNSHIP,
+    APPRENTICE,
+    FREELANCE,
+    TEMPORARY,
+    CONTRACT,
+    REMOTE;
+}
---
+++ ConnectHub/src/main/java/com/frankaboagye/connecthub/enums/ProjectType.java
@@ -0,0 +1,8 @@
+package com.frankaboagye.connecthub.enums;
+
+public enum ProjectType {
+    FULL_TIME,
+    PART_TIME,
+    INTERNSHIP,
+    TEMPORARY;
+}
