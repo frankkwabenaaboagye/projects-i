35d57fa653e91e801a14c30838caaba8a2361681
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-10-12 11:01:27 +0000

    update html - applicationforms for Job, modify modelMap

--- ConnectHub/src/main/java/com/frankaboagye/connecthub/controllers/JobApplicationController.java
+++ ConnectHub/src/main/java/com/frankaboagye/connecthub/controllers/JobApplicationController.java
@@ -2,6 +2,7 @@ package com.frankaboagye.connecthub.controllers;
 
 import com.frankaboagye.connecthub.daos.JobApplicationDAO;
 import com.frankaboagye.connecthub.entities.Freelancer;
+import com.frankaboagye.connecthub.entities.Job;
 import com.frankaboagye.connecthub.entities.JobApplication;
 import com.frankaboagye.connecthub.interfaces.CompanyServiceInterface;
 import com.frankaboagye.connecthub.interfaces.JobApplicationServiceInterface;
@@ -13,10 +14,14 @@ import com.frankaboagye.connecthub.repositories.JobRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @Controller
@@ -35,7 +40,8 @@ public class JobApplicationController {
             @ModelAttribute JobApplicationDAO jobApplicationDAO,
             @PathVariable long jobId,
             HttpSession httpSession,
-            MultipartFile resumeFile
+            MultipartFile resumeFile,
+            ModelMap modelMap
     ){
 
         //TODO
@@ -45,6 +51,37 @@ public class JobApplicationController {
             return "redirect:/login-freelancer";
         }
 
+        Job job = jobRepository.findById(jobId).orElse(null);
+        if (job == null) {
+            return "redirect:/login-freelancer";
+        }
+
+        if(resumeFile.isEmpty()){
+            return "redirect:/login-freelancer";
+        }
+
+        JobApplication jobApplication = JobApplication.builder()
+                .fullName(jobApplicationDAO.getFullName())
+                .email(jobApplicationDAO.getEmail())
+                .linkedIn(jobApplicationDAO.getLinkedIn())
+                .phoneNumber(jobApplicationDAO.getPhoneNumber())
+                .applicationDate(LocalDate.now())
+                .coverLetter(jobApplicationDAO.getCoverLetter())
+                .build();
+
+        // resumeLocation
+        storageServiceImplementation.store(resumeFile);
+        Path resumePath = storageServiceImplementation.load(resumeFile.getOriginalFilename());
+        jobApplication.setResumeLocation(resumePath.toString());
+
+        jobApplicationServiceImplementation.submitJobApplication(jobApplication);
+
+
+        modelMap.addAttribute("freelancer", freelancer);
+        modelMap.addAttribute("job", job);
+
+        httpSession.setAttribute("freelancer", freelancer);
+        httpSession.setAttribute("job", job);
 
         return "successPage";
     }
