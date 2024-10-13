c807aae4d6862557df52126cb13c0aa24b91b8f8
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-10-13 21:08:45 +0000

    fixing exception in module

--- ConnectHub/src/main/java/com/frankaboagye/connecthub/dtos/CompanyDTO.java
+++ ConnectHub/src/main/java/com/frankaboagye/connecthub/dtos/CompanyDTO.java
@@ -1,18 +1,18 @@
-package com.frankaboagye.connecthub.dtos;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class CompanyDTO {
-    private String companyName;
-    private String companyEmail; // we will not update email
-    private String companyPhonenumber;
-    private String companyWebsite;
-    private String companyDescription;
-}
+//package com.frankaboagye.connecthub.dtos;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class CompanyDTO {
+//    private String companyName;
+//    private String companyEmail; // we will not update email
+//    private String companyPhonenumber;
+//    private String companyWebsite;
+//    private String companyDescription;
+//}
