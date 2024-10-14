2482c96aa37f1b20db72504e5f4044abc678b2f3
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-10-14 10:46:02 +0000

    stop tracking connect_hub

--- Connect_Hub/src/main/java/com/frankaboagye/connecthub/interfaces/JobApplicationServiceInterface.java
+++
@@ -1,8 +0,0 @@
-//package com.frankaboagye.connecthub.interfaces;
-//
-//import com.frankaboagye.connecthub.entities.JobApplication;
-//
-//public interface JobApplicationServiceInterface {
-//
-//    void submitJobApplication(JobApplication jobApplication);
-//}
--- Connect_Hub/src/main/java/com/frankaboagye/connecthub/utilities/Helper.java
+++
@@ -1,5 +0,0 @@
-package com.frankaboagye.connecthub.utilities;
-
-public class Helper {
-
-}
